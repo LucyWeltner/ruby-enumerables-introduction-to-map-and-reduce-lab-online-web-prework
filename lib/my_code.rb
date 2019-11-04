8e94ba89cc695ef3087d2f9993d6a3e828bb5aa2
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-04 02:17:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -66,7 +66,7 @@ def reduce_to_any_true(source_array)
   i = 0 
   anyTrue = false
   while i < source_array.length do
-    if source_array[i] == true 
+    if source_array[i] 
       anyTrue = true 
     end 
     i += 1 
